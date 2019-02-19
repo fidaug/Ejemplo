5255014e8a4ac34306799e79776844c13996a24d
Augusto Fidel Barreno Díaz <fidaug@gmail.com>
2019-02-18 20:32:36 -0600

    Agregando Fecha Actual

--- Principal.java
+++ Principal.java
@@ -1,9 +1,22 @@
+import java.util.Date;
+import java.text.DateFormat ;
+import java.text.SimpleDateFormat;
+
 public class Principal{
+
+public void fecha(){
+  Date fecha = new Date();
+  DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+  System.out.println(dateFormat.format(fecha));
+}
 
 public static void main(String ...args){
 
+  Principal run = new Principal();
+
   System.out.println("Hola Mundo");
   System.out.println("Como va la bandera!!");
+  run.fecha();
 }
 
 }
